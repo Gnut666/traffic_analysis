37dd8f902fb743391cbea89a410dbd3dcf594db0
gnut <ludekurban66@gmail.com>
2018-11-01 13:46:47 +0100

    1st

---
+++ traffic_analysis.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class Test:
+	#inin
+	def f(self):
+		return "hell no";
+
+a = Test();
+print(a.f());
+#nacteni
+
+	#loop to end of file
+	##? must know size
+	#save each packet to array
+
+#processing
+	#loop to trough array
+	#sum,count min, max - 1st flag
+	#protocol, data -> protocol data
+	#asoc array(ipI,ipO,prot):data count
+	
+
+	#array end
+	# average(sum,count)
+#vystup
+
