ad215fb51cae1b223c1f97a62d14190cb562d92f
Gnut666 <ludekurban66@gmail.com>
2018-11-05 17:05:01 +0100

    begin interface Load

--- traffic_analysis.py
+++ traffic_analysis.py
@@ -2,15 +2,30 @@
 import os.path
 import sys
 import binascii
+import abc
 
 #loading class
-class Load_pcap:
+class Load( abc.ABC ):
+	@abc.abstractmethod
+	def ld_byte( self, nbyte ):
+		pass;
+	def cut_head_gl( self ):
+		pass;
+class Load_pcap( Load ):
 	
-		#init exit program if file "filename" not exist
-		def __init__( self, filename ):
-			self.open_file = open( filename, 'rb')
-		def ld_byte( self ): 
-			return self.open_file.read(2);	
+	#init exit program if file "filename" not exist
+	def __init__( self, filename ):
+		self.open_file = open( filename, 'rb');
+		self.order = self.cut_head_gl();
+	#todo destruktor
+	def ld_byte( self, nbyte ): 
+		return self.open_file.read( nbyte );
+	def cut_head_gl( self ):
+		#byte order
+		num1 = self.ld_byte( 2 );
+		num2 = self.ld_byte( 2 );
+		tmp = self.ld_byte( 20 );
+		return num1 < num2;
 	
 # main
 ## nacteni jmena souboru a jeho kontrola
@@ -24,9 +39,12 @@ if  not os.access( infname, os.R_OK ):
 	print ( 'non readable/non exits file' );
 	sys.exit( 1 );
 
+##------debug--------------
 a = Load_pcap( infname );
+b = Load_pcap( infname );
 
-print ( binascii.hexlify(a.ld_byte()) );
+print( binascii.hexlify(a.ld_byte( 1 )) );
+print( binascii.hexlify(a.ld_byte( 1 )) );
 
 
 
