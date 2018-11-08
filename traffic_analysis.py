7d30fc6d83331f42c3c661d6514f6c444d8125c3
Gnut666 <ludekurban66@gmail.com>
2018-11-08 16:30:33 +0100

    better ld_head; better num_bin, minor bugs

--- traffic_analysis.py
+++ traffic_analysis.py
@@ -3,12 +3,14 @@ import os.path
 import sys
 import binascii
 import abc
-import math
+#import math
 
-def num_bin( byte_num ):
-	#transform from bytes( little endian ) to int
-	return int.from_bytes( byte_num, byteorder = 'little' );
-
+def num_bin( byte_num, endi ):
+	#transform from bytes( little/big endian ) to int
+	if endi:
+		return int.from_bytes( byte_num, byteorder = 'little' );
+	else:
+		return int.from_bytes( byte_num, byteorder = 'big' );
 #loading class
 class Load_main( abc.ABC ):
 	@abc.abstractmethod
@@ -31,13 +33,13 @@ class Load_main( abc.ABC ):
 		pass;
 class Load_pcap( Load_main ):
 	def __init__( self, filename ):
-		self._open_file = open( filename, 'rb');
+		self._open_file = open( filename, 'rb' );
 		#save size of file
 		self._size = os.stat(filename).st_size;
 		#save byte arangement in file
 		self._order = self._cut_head_gl();
 		#flag of end of file
-		self._flag_eof = 0;	
+		self.flag_eof = 1;	
 
 	def __del__( self ):
 		self._open_file.close();
@@ -45,11 +47,11 @@ class Load_pcap( Load_main ):
 	def _ld_byte( self, nbyte ):
 		#load nbyte number of bytes
 		if ( self._size - nbyte ) < 0:
-			print ('unexpected end of file');
+			print ( 'unexpected end of file' );
 			sys.exit( 2 );
 		elif ( self._size - nbyte ) == 0:
 			#normal end of file
-			self._flag_eof = 1;
+			self.flag_eof = 0;
 		self._size -= nbyte;
 		return self._open_file.read( nbyte );
 
@@ -61,24 +63,22 @@ class Load_pcap( Load_main ):
 		#header remains
 		tmp = self._ld_byte( 20 );
 		if num1 < num2:
-			self._order = 0;
+			return  0;
 		else:
-			self._order = 1;
-		return 1;
+			return 1;
 	def _ld_head( self ):
 		#read packet header 
 		tmp = self._ld_byte( 8 );
-		oc1 = self._ld_byte( 2 );
-		oc2 = self._ld_byte( 2 );
-		lng1 = self._ld_byte( 2 );
-		lng2 = self._ld_byte( 2 );
+		oc = self._ld_byte( 4 );
+		lng = self._ld_byte( 4 );
+
 		#debug num octtets x actual lenght
-		if ( oc1 != lng1 ) or ( oc2 != lng2 ):
-			print ('number of octets != actual lenght ');
+		if ( oc != lng ):
+			print ( 'warning number of octets != actual lenght' );
 		if ( self._order ):
-			return int( num_bin( lng2 ) * math.pow(2,8) + num_bin( lng1 ));
+			return int( num_bin( lng, 1  ));
 		else:
-			return int( num_bin( lng1 ) * math.pow(2,8) + num_bin( lng2 ));
+			return int( num_bin( lng, 0  ));
 	def ld_packet( self ):
 		# returt whole packet
 		return self._ld_byte( self._ld_head() );		
@@ -100,9 +100,9 @@ if not os.access( infname, os.R_OK ):
 a = Load_pcap( infname );
 #b = Load_pcap( infname );
 
-f = open( 'zz.txt', 'wb' );
-f.write( a.ld_packet() );
-
+#f = open( 'zz.txt', 'wb' );
+#f.write( a.ld_packet() );
+print ( a._ld_head() );
 #print( binascii.hexlify(a._ld_byte( 1 )) );
 
 
