f4f2a3c543b9f61b4b99e05c661ddd7b3a16fab1
Gnut666 <ludekurban66@gmail.com>
2018-11-09 16:34:28 +0100

    process class-> print min,max

--- traffic_analysis.py
+++ traffic_analysis.py
@@ -20,17 +20,16 @@ class Load_main( abc.ABC ):
 	def __del__( self ):
 		pass;
 	@abc.abstractmethod
-	def _ld_byte( self, nbyte ):
+	def get_packet( self ):
 		pass;
 	@abc.abstractmethod
-	def _cut_head_gl( self ):
+	def get_last_size( self ):
 		pass;
 	@abc.abstractmethod
-	def _ld_head( self ):
+	def get_flag_eof( self ):
 		pass;
-	@abc.abstractmethod
-	def ld_packet( self ):
-		pass;
+
+
 class Load_pcap( Load_main ):
 	def __init__( self, filename ):
 		self._open_file = open( filename, 'rb' );
@@ -39,7 +38,9 @@ class Load_pcap( Load_main ):
 		#save byte arangement in file
 		self._order = self._cut_head_gl();
 		#flag of end of file
-		self.flag_eof = 1;	
+		self.flag_eof = 1;
+		#set last size
+		self._last_size = 0;
 
 	def __del__( self ):
 		self._open_file.close();
@@ -76,45 +77,102 @@ class Load_pcap( Load_main ):
 		if ( oc != lng ):
 			print ( 'warning number of octets != actual lenght' );
 		if ( self._order ):
-			return int( num_bin( lng, 1  ));
+			return int( num_bin( lng, 1 ));
 		else:
-			return int( num_bin( lng, 0  ));
-	def ld_packet( self ):
+			return int( num_bin( lng, 0 ));
+	def get_packet( self ):
+		self._last_size = self._ld_head();
 		# returt whole packet
-		return self._ld_byte( self._ld_head() );		
+		return self._ld_byte( self._last_size );
+				
+	def get_last_size( self ):
+		return self._last_size;
+
+	def get_flag_eof( self ):
+                return self.flag_eof;
+
+##-----processing-----
+
+class  Processing_main( abc.ABC ):
+	@abc.abstractmethod
+	def __init__( self ):
+		pass;
+	@abc.abstractmethod
+	def __del__( self ):
+		pass;
+	@abc.abstractmethod
+	def write_report( self ):
+		pass;
+
+class Processing_pcap( Processing_main ):
+	def __init__( self ):
+		## load file name and make control
+		if len( sys.argv ) < 2:
+			print ( 'no file to process' );
+			sys.exit( 1 );
+		filename = sys.argv[ 1 ];
+		# exit program if file "filename" not exist
+		if not os.access( filename, os.R_OK ):
+			print ( 'non readable/non exits file' );
+			sys.exit( 1 );
+		self.pcap = Load_pcap( filename );
+
+	def __del__( self ):
+		del self.pcap;
+	def _proc_packet( self ):
+		self.pcap.get_packet();
+		#TODO protocool, address... 
+		pass;
+	def write_report( self ):
+		size_min = 0;
+		size_max = 0;
+		#total size
+		size_all = 0;
+		packet_count = 0;
+		while self.pcap.get_flag_eof():
+			self._proc_packet();
+			#print ( self.pcap.get_last_size() );
+			last_size = self.pcap.get_last_size();
+			packet_count += 1;
+			if ( packet_count == 1 ):
+				size_max = last_size;
+				size_min = last_size;
+			size_all += last_size;
+			if ( size_max < last_size ):
+				size_max = last_size;
+			if ( size_min > last_size ):
+				size_min = last_size;
+		print ( size_min, size_max, size_all /  packet_count );
+		return 1;
+
+        #sum,count min, max - 1st flag
+        #protocol, data -> protocol data
+        #asoc array(ipI,ipO,prot):data count
 
 
-# main
-## nacteni jmena souboru a jeho kontrola
-if len( sys.argv ) < 2:
-	print ( 'no file to process' );
-	sys.exit( 1 );
+        #array end
+        # average(sum,count)
+	
 
-infname = sys.argv[1];
-# exit program if file "filename" not exist
-if not os.access( infname, os.R_OK ):
-	print ( 'non readable/non exits file' );
-	sys.exit( 1 );
+
+
+##------main------------
+
+a = Processing_pcap();
 
 ##------debug--------------
-a = Load_pcap( infname );
+
 #b = Load_pcap( infname );
 
+a.write_report();
+#print ( a.get_last_size());
+
+#print ( a._ld_head() );
 #f = open( 'zz.txt', 'wb' );
 #f.write( a.ld_packet() );
-print ( a._ld_head() );
 #print( binascii.hexlify(a._ld_byte( 1 )) );
 
 
 
-#processing
-	#loop to trough array
-	#sum,count min, max - 1st flag
-	#protocol, data -> protocol data
-	#asoc array(ipI,ipO,prot):data count
-	
-
-	#array end
-	# average(sum,count)
 #vystup
 
